730299e2d4d3977c65113dec5d50d63cce986760
parvejleelgar <parvejchauhan23@gmail.com>
2019-09-16 12:15:47 +0530

    Change in Date Validaton and handle NPE. Remove static message from view

--- modules/event-registration-module/src/main/java/com/axelor/event/registration/db/report/ITranslation.java
+++ modules/event-registration-module/src/main/java/com/axelor/event/registration/db/report/ITranslation.java
@@ -7,4 +7,13 @@ public interface ITranslation {
 	public static final String DATE_BETWEEN = /*$$(*/ "Registration Date Can not between Event Start Date and End Date"; /*)*/
 	
 	public static final String MISSING_FIELD = /*$$(*/ "Please fill Registration Date and Event field"; /*)*/
+	
+	public static final String MISSING_REGISTRATION_DATE = /*$$(*/ "Please fill Registration Date "; /*)*/
+	
+	public static final String START_DATE = /*$$(*/ "start date should not be ahead of endDate"; /*)*/
+	
+	public static final String REGISTRATION_OPEN = /*$$(*/ "Registration Open should not be ahead of registration close"; /*)*/
+	
+	public static final String REGISTRATION_CLOSE = /*$$(*/ "Registration close date must be before event start date"; /*)*/
+
 }
--- modules/event-registration-module/src/main/java/com/axelor/event/registration/db/web/EventController.java
+++ modules/event-registration-module/src/main/java/com/axelor/event/registration/db/web/EventController.java
@@ -2,8 +2,11 @@ package com.axelor.event.registration.db.web;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
 
+import com.axelor.event.registration.db.Discount;
 import com.axelor.event.registration.db.Event;
 import com.axelor.event.registration.db.EventRegistration;
 import com.axelor.event.registration.db.report.ITranslation;
@@ -15,19 +18,60 @@ public class EventController {
 
 	public void validation(ActionRequest request, ActionResponse response) {
 		Event event = request.getContext().asType(Event.class);
-
 		if (event.getCapacity() < event.getTotalEntry()) {
 			response.setError(I18n.get(ITranslation.CAPACITY_EXCEED));
 		} else {
 			LocalDate registrationOpen = event.getRegistrationOpen();
 			LocalDate registrationClose = event.getRegistrationClose();
-
-			for (EventRegistration eventRegistration : event.getEventRegistrationList()) {
-				LocalDate registrationDate = eventRegistration.getRegistrationDate().toLocalDate();
-				if (registrationOpen != null && registrationClose != null && registrationDate != null
-						&& registrationDate.isBefore(registrationClose) && registrationDate.isAfter(registrationOpen)) {
-					response.setError(I18n.get(ITranslation.DATE_BETWEEN));
+			if (event.getEventRegistrationList() != null) {
+				for (EventRegistration eventRegistration : event.getEventRegistrationList()) {
+					if (eventRegistration.getRegistrationDate() != null) {
+						LocalDate registrationDate = eventRegistration.getRegistrationDate().toLocalDate();
+						if (registrationOpen != null && registrationClose != null && registrationDate != null
+								&& registrationDate.isBefore(registrationClose)
+								&& registrationDate.isAfter(registrationOpen)) {
+							response.setError(I18n.get(ITranslation.DATE_BETWEEN));
+						}
+					} else {
+						response.setError(I18n.get(ITranslation.MISSING_REGISTRATION_DATE));
+					}
 				}
+			}
+		}
+	}
+
+	public void dateValidation(ActionRequest request, ActionResponse response) {
+		Event event = request.getContext().asType(Event.class);
+		LocalDate startDate = null;
+		LocalDate endDate = null;
+
+		if (event.getStartDate() != null) {
+			startDate = event.getStartDate().toLocalDate();
+			if (event.getEndDate() != null) {
+				endDate = event.getEndDate().toLocalDate();
+				if (startDate.isAfter(endDate)) {
+					response.setFlash(I18n.get(ITranslation.START_DATE));
+					response.setValue("startDate", null);
+					response.setValue("endDate", null);
+				}
+			}
+		}
+
+		LocalDate registrationOpen = event.getRegistrationOpen();
+		LocalDate registrationClose = event.getRegistrationClose();
+		if (registrationOpen != null && registrationClose != null) {
+			if (registrationOpen.isAfter(registrationClose)) {
+				response.setFlash(I18n.get(ITranslation.REGISTRATION_OPEN));
+				response.setValue("registrationOpen", null);
+				response.setValue("registrationClose", null);
+			}
+		}
+
+		if (registrationClose != null && startDate != null) {
+			if (registrationClose.isAfter(startDate)) {
+				response.setFlash(I18n.get(ITranslation.REGISTRATION_CLOSE));
+				response.setValue("startDate", null);
+				response.setValue("registrationClose", null);
 			}
 		}
 	}
