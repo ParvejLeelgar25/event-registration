907e94b576ab210b8f6241253da893ee4228caaa
parvejleelgar <parvejchauhan23@gmail.com>
2019-09-19 13:39:13 +0530

    card view for event

--- modules/event-registration-module/src/main/java/com/axelor/event/registration/db/module/EventRegistrationModule.java
+++ modules/event-registration-module/src/main/java/com/axelor/event/registration/db/module/EventRegistrationModule.java
@@ -4,7 +4,6 @@ import com.axelor.app.AxelorModule;
 import com.axelor.event.registration.db.Event;
 import com.axelor.event.registration.db.repo.AddressManageRepository;
 import com.axelor.event.registration.db.repo.AddressRepository;
-import com.axelor.event.registration.db.repo.EventManageRepository;
 import com.axelor.event.registration.db.repo.EventRegistrationManageRepository;
 import com.axelor.event.registration.db.repo.EventRegistrationRepository;
 import com.axelor.event.registration.db.repo.EventRepository;
@@ -22,7 +21,6 @@ public class EventRegistrationModule extends AxelorModule{
 		bind(AddressService.class).to(AddressServiceImpl.class);
 		bind(AddressRepository.class).to(AddressManageRepository.class);
 		bind(EventRegistrationRepository.class).to(EventRegistrationManageRepository.class);
-		bind(EventRepository.class).to(EventManageRepository.class);
 		bind(EventService.class).to(EventServiceImpl.class);
 		bind(EventRegistrationService.class).to(EventRegistrationServiceImpl.class);
 		
